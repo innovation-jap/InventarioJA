6376d7dd51fd39dbbdbbe6861665ccf97406b36d
svMatheu <matiasalcala123456@gmail.com>
2026-01-07 01:25:36 -0500

    primeros cambios para la migracion a AtlasDB

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM php:8.2-apache
 
-# Instalar extensiones necesarias para PDO MySQL
-RUN docker-php-ext-install pdo pdo_mysql
+# Instalar dependencias para la extensión de MongoDB
+RUN apt-get update && apt-get install -y \
+    libcurl4-openssl-dev pkg-config libssl-dev
 
-# Habilitar mod_rewrite (si luego usas .htaccess)
+# Instalar y habilitar la extensión de MongoDB
+RUN pecl install mongodb && docker-php-ext-enable mongodb
+
+# Habilitar mod_rewrite
 RUN a2enmod rewrite
 
-# Copiar todo el proyecto al docroot de Apache
 COPY . /var/www/html
-
-# Directorio de trabajo
-WORKDIR /var/www/html  
+WORKDIR /var/www/html
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,22 +6,49 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
 {
+    public static $files = array (
+        '606a39d89246991a373564698c2d8383' => __DIR__ . '/..' . '/symfony/polyfill-php85/bootstrap.php',
+        '3a37ebac017bc098e9a86b35401e7a68' => __DIR__ . '/..' . '/mongodb/mongodb/src/functions.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'S' =>
+        array (
+            'Symfony\\Polyfill\\Php85\\' => 23,
+        ),
         'P' =>
         array (
+            'Psr\\Log\\' => 8,
             'PHPMailer\\PHPMailer\\' => 20,
+        ),
+        'M' =>
+        array (
+            'MongoDB\\' => 8,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php85\\' =>
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php85',
+        ),
+        'Psr\\Log\\' =>
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/src',
+        ),
         'PHPMailer\\PHPMailer\\' =>
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+        'MongoDB\\' =>
+        array (
+            0 => __DIR__ . '/..' . '/mongodb/mongodb/src',
         ),
     );
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'NoDiscard' => __DIR__ . '/..' . '/symfony/polyfill-php85/Resources/stubs/NoDiscard.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
