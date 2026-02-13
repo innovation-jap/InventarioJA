b3f0c4e42f778c29281a7b4e45c61318817f495f
svMatheu <matiasalcala123456@gmail.com>
2026-02-13 16:41:29 -0500

    cambios base en movimientos

--- Vista/vistaMovimientos.php
+++ Vista/vistaMovimientos.php
@@ -24,7 +24,7 @@ $current_fecha_fin = $_GET['fecha_fin'] ?? '';
 <head>
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-    <title>Movimientos - Donde Patty</title>
+    <title>Movimientos - Junnior Achievement</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
